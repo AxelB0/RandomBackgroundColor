64a3c6b17cea4041ac3dd6f6ed2d3e8416ef0752
AxelBO <axel.boonen.mail@gmail.com>
2025-04-30 11:05:09 +0200

    added loop in newColorHex function

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ console.log('Happy developing ✨')
 let colorHex = newColorHex();
 
 function newColorHex() {
-    let number1 = generateNumber().toString(16);
-    let number2 = generateNumber().toString(16);
-    let number3 = generateNumber().toString(16);
-    let number4 = generateNumber().toString(16);
-    let number5 = generateNumber().toString(16);
-    let number6 = generateNumber().toString(16);
-    return `#${number1}${number2}${number3}${number4}${number5}${number6}`;
+    let generatedColor = '#';
+    for (let i = 0; i < 6; i++) {
+        generatedColor += generateNumber().toString(16);
+    }
+    return generatedColor;
 
 }
-function generateNumber(){
+
+function generateNumber() {
     return Math.floor(Math.random() * 16);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-
     document.getElementById('box').style.backgroundColor = colorHex;
     document.getElementById('box').innerText = colorHex;
 })
